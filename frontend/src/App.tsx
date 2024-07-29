07460c80b77eef65192474c53cff327e3a01ff92
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-07-29 15:29:01 +0900

    test12

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,17 +8,24 @@ function App() {
       <div>ふざけるのをやめました</div>
       <div>ECRにPushして自動でデプロイかかったら最高</div>
       <div>なかなかうまくいかんね！！</div>
-        <div>IAMロール既存の使うと怒られる。自分で作った</div>
-        <div>一旦、リソースの作成、ECRPush後の自動デプロイはOK。次はCodePipeline組もう！！</div>
-        <div>deployが成功したり、失敗したりで安定しないのはなんで？</div>
-        <div>CPU,memoryを増やしたら安定するって聞いた</div>
-        <div>なんか、githubから直接ECRにPushかけて、それを元にECSにデプロイするっぽい</div>
-        <div>あんまりGithubActionsからの動きうまくいってなさそう</div>
-        <div>task定義をgithub-ci.ymlに書くのは後でやめたいね</div>
-        <div>やっぱりデプロイの自動化は難しいね。早く次のステップに行きたい</div>
-        <div>CPUメモリのサイズの問題ではなさそう。なぜデプロイが失敗するのか。。。</div>
-        <div>terminalから直接。。。</div>
-        <div>タスクロールが抜けてたかも。。。。</div>
+      <div>IAMロール既存の使うと怒られる。自分で作った</div>
+      <div>
+        一旦、リソースの作成、ECRPush後の自動デプロイはOK。次はCodePipeline組もう！！
+      </div>
+      <div>deployが成功したり、失敗したりで安定しないのはなんで？</div>
+      <div>CPU,memoryを増やしたら安定するって聞いた</div>
+      <div>
+        なんか、githubから直接ECRにPushかけて、それを元にECSにデプロイするっぽい
+      </div>
+      <div>あんまりGithubActionsからの動きうまくいってなさそう</div>
+      <div>task定義をgithub-ci.ymlに書くのは後でやめたいね</div>
+      <div>やっぱりデプロイの自動化は難しいね。早く次のステップに行きたい</div>
+      <div>
+        CPUメモリのサイズの問題ではなさそう。なぜデプロイが失敗するのか。。。
+      </div>
+      <div>terminalから直接。。。</div>
+      <div>タスクロールが抜けてたかも。。。。</div>
+      <div>なぜかimage_uriが取れてなかっただけ</div>
       <br />
       <br />
       <div>コードはBuildされたものをECRへPushしてます</div>
